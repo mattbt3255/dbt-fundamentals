4d0ec7548abcf34eaed45479c52e1cae24cee02b
Matt Brown <mattbt3255@gmail.com>
2022-01-07 15:47:26 +0000

    added stripe source yml file

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -7,7 +7,7 @@ with payments as (
         amount / 100.0 as amount, -- amount is stored in cents, convert it to dollars
         created as created_at
 
-    from raw.stripe.payment
+    from {{source('stripe','payment')}}
 )
 
 select * from payments
